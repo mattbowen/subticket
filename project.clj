a03fe342c37f508fc00e5f74104ff625d41c59c6
Matt Bowen <mrbowen@gmail.com>
2015-10-28 20:17:29 -0400

    Fill our project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject subticket "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A radically simple ticketing system"
+  :url "https://github.com/mattbowen/subticket"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
