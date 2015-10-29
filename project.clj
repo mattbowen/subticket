d190389919ccd72a573c8e9d87cd55509efba363
Matt Bowen <mrbowen@gmail.com>
2015-10-28 20:33:26 -0400

    Add database deps

--- project.clj
+++ project.clj
@@ -15,7 +15,11 @@
                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.7"]
                  [org.slf4j/jcl-over-slf4j "1.7.7"]
-                 [org.slf4j/log4j-over-slf4j "1.7.7"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.7"]
+                 [com.mchange/c3p0 "0.9.2.1"] ; connection pooling
+                 [clj-liquibase "0.5.3"]  ; migrations!
+                 [oss-jdbc      "0.8.0"]  ; for Open Source JDBC drivers
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "subticket.server/run-dev"]}
