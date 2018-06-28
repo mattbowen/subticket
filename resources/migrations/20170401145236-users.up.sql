086f54145568ca80d0e8ddcea137da03e0ebd6bd
Patrick Curran <patrickthebold@gmail.com>
2018-06-28 16:58:57 -0400

    More specific naming.

--- resources/migrations/20170401145236-users.up.sql
+++ resources/migrations/20170401145236-users.up.sql
@@ -11,52 +11,52 @@ CREATE TABLE namespaces (
     name varchar(64),
     write varchar(64) NOT NULL,
     PRIMARY KEY (name),
-    FOREIGN KEY (name, write) REFERENCES sets (namespace, name) -- TODO on delete.
+    FOREIGN KEY (name, write) REFERENCES user_sets (namespace, name) -- TODO on delete.
 );
 
 CREATE TYPE set_type as ENUM ('concrete', 'expression');
 
-CREATE TABLE sets (
+CREATE TABLE user_sets (
     namespace varchar(64),
     name varchar(64),
     type set_type NOT NULL,
     write varchar(64) NOT NULL,
     PRIMARY KEY (namespace, name),
-    FOREIGN KEY (namespace, write) REFERENCES sets (namespace, name) -- TODO on delete
+    FOREIGN KEY (namespace, write) REFERENCES user_sets (namespace, name) -- TODO on delete
 );
 
-CREATE TABLE concrete_sets (
+CREATE TABLE concrete_user_sets (
     namespace varchar(64),
     name varchar(64),
     username varchar(64),
     PRIMARY KEY (namespace, name, username),
-    FOREIGN KEY (namespace, name) REFERENCES sets (namespace, name), -- TODO check type.
+    FOREIGN KEY (namespace, name) REFERENCES user_sets (namespace, name), -- TODO check type.
     FOREIGN KEY (username) REFERENCES users (username) -- TODO on delete
 );
 
-CREATE TABLE expression_sets (
+CREATE TABLE expression_user_sets (
     namespace varchar(64),
     name varchar(64),
     literal jsonb,
     PRIMARY KEY (namespace, name),
-    FOREIGN KEY (namespace, name) REFERENCES sets (namespace, name), -- TODO check type.
+    FOREIGN KEY (namespace, name) REFERENCES user_sets (namespace, name), -- TODO check type.
 );
 
-CREATE TABLE child_sets (
+CREATE TABLE child_user_sets (
     namespace varchar(64),
     parent varchar(64),
     child varchar(64),
     PRIMARY KEY (namespace, parent, child),
-    FOREIGN KEY (namespace, parent) REFERENCES expression_sets (namespace, name),
-    FOREIGN KEY (namespace, child) REFERENCES sets (namespace, name)
+    FOREIGN KEY (namespace, parent) REFERENCES expression_user_sets (namespace, name),
+    FOREIGN KEY (namespace, child) REFERENCES user_sets (namespace, name)
 );
 
-CREATE TABLE anf_terms (
+CREATE TABLE user_anf_terms (
     namespace varchar(64),
     name varchar(64),
     term integer,
     set varchar(64),
     PRIMARY KEY (namespace, name, term, set),
-    FOREIGN KEY (namespace, name) REFERENCES expression_sets (namespace, name),
-    FOREIGN KEY (namespace, set) REFERENCES sets (namespace, name)
+    FOREIGN KEY (namespace, name) REFERENCES expression_user_sets (namespace, name),
+    FOREIGN KEY (namespace, set) REFERENCES user_sets (namespace, name)
 );
